11ffdd296c49cad0e64c66d1d110e6f9207a0f02
Eduardo Turella <eduardohe@gmail.com>
2017-10-09 19:52:30 -0300

    Setting project up

---
+++ src/main/java/com/easytravel/EasytravelApplication.java
@@ -0,0 +1,12 @@
+package com.easytravel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasytravelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasytravelApplication.class, args);
+	}
+}
